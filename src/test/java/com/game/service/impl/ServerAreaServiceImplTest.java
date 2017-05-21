71d0b67fae827fe1aa72134f41ee1f3e32e4b214
jeniss <jenisstian@hotmail.com>
2017-05-21 18:58:03 +0800

    mapper

--- src/test/java/com/game/service/impl/ServerAreaServiceImplTest.java
+++ src/test/java/com/game/service/impl/ServerAreaServiceImplTest.java
@@ -4,24 +4,20 @@ import com.game.model.ServerArea;
 import com.game.service.ServerAreaService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 /**
  * Created by jennifert on 5/18/2017.
  */
-@Component
-@Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext.xml")
 public class ServerAreaServiceImplTest {
-    @Resource
+    @Autowired
     private ServerAreaService serverAreaService;
 
     @Test
