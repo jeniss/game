110c2d451b22fc1ce3ff02be3f60a03f89f4cf29
Jennifer Tian <jennifert@synnex.com>
2017-06-27 15:24:33 +0800

    handle the exception

--- src/main/java/com/game/thread/ProcessDataThread.java
+++ src/main/java/com/game/thread/ProcessDataThread.java
@@ -34,16 +34,12 @@ public class ProcessDataThread extends Thread {
         IServerAreaService serverAreaService = (IServerAreaService) SpringContextUtil.getBean("serverAreaService");
         IGameService gameService = (IGameService) SpringContextUtil.getBean("gameService");
         IGameCategoryService gameCategoryService = (IGameCategoryService) SpringContextUtil.getBean("gameCategoryService");
-        ITradeFlowService tradeFlowService = (ITradeFlowService) SpringContextUtil.getBean("tradeFlowService");
 
         try {
             List<ServerArea> serverAreaList = serverAreaService.getAll();
 
             List<Game> gameList = gameService.getAllGameList();
             List<GameCategory> gameCategoryList = gameCategoryService.getAllGameCategory();
-
-            String urlPrefix = "http://www.uu898.com/newTrade.aspx?";
-
 
             // Traversal the game
             for (Game game : gameList) {
@@ -53,81 +49,12 @@ public class ProcessDataThread extends Thread {
                     for (ServerArea serverArea : serverAreaList) {
                         // Traversal the childServer area of game
                         for (ServerArea childServer : serverArea.getChildServerAreas()) {
-                            List<TradeFlow> tradeFlowList = new ArrayList<>();
-                            // process the url
-                            StringBuilder stringBuilder = new StringBuilder(urlPrefix);
-                            stringBuilder.append("gm=" + game.getCode());
-                            stringBuilder.append("&c=" + gameCategory.getCode());
-                            stringBuilder.append("&area=" + serverArea.getCode());
-                            stringBuilder.append("&srv=" + childServer.getCode());
-                            String urlStr = stringBuilder.toString();
-
-                            // get the first page info
-                            Document document = Jsoup.connect(urlStr).timeout(10 * 1000).get();
-                            Elements contentElement = document.select("div[id=divCommodityLst] ul");
-                            if (contentElement != null) {
-                                for (Element element : contentElement) {
-                                    logger.info("---------------------------");
-                                    logger.info(element.html());
-                                    logger.info("---------------------------");
-                                    TradeFlow tradeFlow = new TradeFlow();
-                                    // name
-                                    String name = element.select("li[class=sp_li0 pos] h2 a").text();
-                                    tradeFlow.setName(name);
-
-                                    // price
-                                    double price = Double.valueOf(element.select("li[class=Red zuan_dh] span").text());
-                                    tradeFlow.setPrice(price);
-
-                                    // stock
-                                    String stock = element.select("li[class=sp_li3] h5").text();
-                                    if (StringUtil.isNumeric(stock)) {
-                                        tradeFlow.setStock(Integer.valueOf(stock));
-                                    } else {
-                                        tradeFlow.setStock(0);
-                                    }
-
-                                    // totalPrice
-                                    Double totalPrice = tradeFlow.getPrice() * tradeFlow.getStock();
-                                    tradeFlow.setTotalPrice(totalPrice);
-
-                                    // unitPrice
-                                    String unitPrice = element.select("li[class=sp_li1] h6 span").first().text();
-                                    tradeFlow.setUnitPrice(unitPrice);
-
-                                    // tradeStatus
-                                    String tradeStatus = element.select("li[class=sp_li1] a").text();
-                                    // trading
-                                    if (StringUtils.isEmpty(tradeStatus)) {
-                                        tradeStatus = element.select("li[class=sp_li1]>span[class=btn_jyz]").text();
-                                    }
-                                    // finished
-                                    if (StringUtils.isEmpty(tradeStatus)) {
-                                        tradeStatus = element.select("li[class=sp_li1]>span[class=btn_jywc]").text();
-                                    }
-                                    logger.info(tradeStatus);
-                                    tradeFlow.setTradeStatus(TradeStatusType.getTradeStatusTypeByDesc(tradeStatus).name());
-
-                                    tradeFlow.setGame(game);
-                                    tradeFlow.setServerArea(childServer);
-                                    tradeFlow.setGameCategory(gameCategory);
-
-                                    tradeFlowList.add(tradeFlow);
-                                }
-
-                                if (!CollectionUtils.isEmpty(tradeFlowList)) {
-                                    tradeFlowService.postTradeFlowBatch(tradeFlowList);
-
-                                    String msg = "--------------------------- area:%s,server:%s,category:%s,size:%s ---------------------------";
-                                    logger.info(String.format(msg, serverArea.getId(), childServer.getId(), gameCategory.getId(), tradeFlowList.size()));
-                                    Random random = new Random();
-                                    int sleepTime = (random.nextInt(30) + 30) * 1000;// 30s ~ 60s
-                                    Thread.sleep(sleepTime);
-                                } else {
-                                    logger.info("--------------------------- size:0," + urlStr);
-                                }
-                            } else {
-                                logger.info("--------------------------- size:0," + urlStr);
+                            try {
+                                this.processData(game, gameCategory, serverArea, childServer);
+                            } catch (Exception e) {
+                                String msg = "area:%s,server:%s,category:%s";
+                                logger.error(Thread.currentThread().getStackTrace()[0].getMethodName() + ", " + String.format(msg, serverArea.getId(), childServer.getId(),
+                                        gameCategory.getId()), e);
                             }
                         }
                     }
@@ -135,6 +62,95 @@ public class ProcessDataThread extends Thread {
             }
         } catch (Exception e) {
             logger.error(Thread.currentThread().getStackTrace()[0].getMethodName(), e);
+        }
+    }
+
+    /**
+     * process the data of html, save it to DB
+     * @param game
+     * @param gameCategory
+     * @param serverArea
+     * @param childServer
+     * @throws Exception
+     */
+    private void processData(Game game, GameCategory gameCategory, ServerArea serverArea, ServerArea childServer) throws Exception {
+        String urlPrefix = "http://www.uu898.com/newTrade.aspx?";
+        ITradeFlowService tradeFlowService = (ITradeFlowService) SpringContextUtil.getBean("tradeFlowService");
+
+        List<TradeFlow> tradeFlowList = new ArrayList<>();
+        // process the url
+        StringBuilder stringBuilder = new StringBuilder(urlPrefix);
+        stringBuilder.append("gm=" + game.getCode());
+        stringBuilder.append("&c=" + gameCategory.getCode());
+        stringBuilder.append("&area=" + serverArea.getCode());
+        stringBuilder.append("&srv=" + childServer.getCode());
+        String urlStr = stringBuilder.toString();
+
+        // get the first page info
+        Document document = Jsoup.connect(urlStr).timeout(10 * 1000).get();
+        Elements contentElement = document.select("div[id=divCommodityLst] ul");
+        if (contentElement != null) {
+            for (Element element : contentElement) {
+                logger.info("--------------------------- html content start ---------------------------");
+                logger.info(element.html());
+                logger.info("--------------------------- html content end ---------------------------");
+                TradeFlow tradeFlow = new TradeFlow();
+                // name
+                String name = element.select("li[class=sp_li0 pos] h2 a").text();
+                tradeFlow.setName(name);
+
+                // price
+                double price = Double.valueOf(element.select("li[class=Red zuan_dh] span").text());
+                tradeFlow.setPrice(price);
+
+                // stock
+                String stock = element.select("li[class=sp_li3] h5").text();
+                if (StringUtil.isNumeric(stock)) {
+                    tradeFlow.setStock(Integer.valueOf(stock));
+                } else {
+                    tradeFlow.setStock(0);
+                }
+
+                // totalPrice
+                Double totalPrice = tradeFlow.getPrice() * tradeFlow.getStock();
+                tradeFlow.setTotalPrice(totalPrice);
+
+                // unitPrice
+                String unitPrice = element.select("li[class=sp_li1] h6 span").first().text();
+                tradeFlow.setUnitPrice(unitPrice);
+
+                // tradeStatus
+                String tradeStatus = element.select("li[class=sp_li1] a").text();
+                // trading
+                if (StringUtils.isEmpty(tradeStatus)) {
+                    tradeStatus = element.select("li[class=sp_li1]>span[class=btn_jyz]").text();
+                }
+                // finished
+                if (StringUtils.isEmpty(tradeStatus)) {
+                    tradeStatus = element.select("li[class=sp_li1]>span[class=btn_jywc]").text();
+                }
+                tradeFlow.setTradeStatus(TradeStatusType.getTradeStatusTypeByDesc(tradeStatus).name());
+
+                tradeFlow.setGame(game);
+                tradeFlow.setServerArea(childServer);
+                tradeFlow.setGameCategory(gameCategory);
+
+                tradeFlowList.add(tradeFlow);
+            }
+
+            if (!CollectionUtils.isEmpty(tradeFlowList)) {
+                tradeFlowService.postTradeFlowBatch(tradeFlowList);
+
+                String msg = "--------------------------- area:%s,server:%s,category:%s,size:%s ---------------------------";
+                logger.info(String.format(msg, serverArea.getId(), childServer.getId(), gameCategory.getId(), tradeFlowList.size()));
+                Random random = new Random();
+                int sleepTime = (random.nextInt(30) + 30) * 1000;// 30s ~ 60s
+                Thread.sleep(sleepTime);
+            } else {
+                logger.info("--------------------------- content size:0," + urlStr);
+            }
+        } else {
+            logger.info("--------------------------- content size:0," + urlStr);
         }
     }
 }
