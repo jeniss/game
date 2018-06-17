db7969e2a821f07e4af547f066e9eee80aa66c27
jeniss <jenisstian@hotmail.com>
2018-06-17 22:11:55 +0800

    update wait time

--- src/main/java/com/game/service/impl/SeleniumProcessHTMLServiceServiceImpl.java
+++ src/main/java/com/game/service/impl/SeleniumProcessHTMLServiceServiceImpl.java
@@ -142,7 +142,7 @@ public class SeleniumProcessHTMLServiceServiceImpl implements ISeleniumProcessHT
                 redisCache.set(RedisKey.CRON_EXCEPTION_FLAG, "Y");
                 if (exceptionMsg.contains("org.apache.http.conn.HttpHostConnectException")) {
                     ghostWebDriver.quit();
-                    int waitTime = 1000 * 60 * 5;
+                    int waitTime = 1000 * 60 * 2;
                     this.waitForAWhile(waitTime);
                 }
             }
